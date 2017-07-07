bad409e2a4ca6584f987f1989443e547ac792574
PalakJoshi04 <palakjoshi1993@gmail.com>
2017-07-07 11:50:22 -0400

    Update calcareaoftriangle.py

--- calcareaoftriangle.py
+++ calcareaoftriangle.py
@@ -2,4 +2,5 @@ area = 0
 height = 7
 base = 3
 area = height * base /2
-print('the area of triangle is %f' %area)
+print('the area of triangle is %f' %area) # %f is used for float coz 7*3 =21 gives decimal value 
+                                (#%.2f can be used if only 2 decimal places are desired)
