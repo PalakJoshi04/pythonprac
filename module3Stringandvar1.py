93e41ad2a118ae3ddedac2c1a15d5e4a29d89ff1
PalakJoshi04 <palakjoshi1993@gmail.com>
2017-07-07 11:24:56 -0400

    Add files via upload

---
+++ module3Stringandvar1.py
@@ -0,0 +1,16 @@
+name = input("what is your name? ")
+name = name.upper()
+country = input("what country do u live in? ")
+country = country.upper()
+print(name + " lives in " + " USA")
+
+message = 'hello world'
+message = message.upper()
+print(message)
+
+#or
+message = 'hello world'
+print(message.upper())
+print(message.capitalize())
+print(message.replace('hello', 'hi'))
+print(message.count('o'))
