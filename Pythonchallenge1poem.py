73d6750b1b1b23557e96241d8cf9ee4e40546adb
PalakJoshi04 <palakjoshi1993@gmail.com>
2017-07-07 11:37:19 -0400

    Update Pythonchallenge1poem.py

--- Pythonchallenge1poem.py
+++ Pythonchallenge1poem.py
@@ -1,1 +1,3 @@
+# this program prints the poem using \n
+
 print("There once was a movie star icon \nwho preferred to sleep with the light on \nThey learned how to code \na device that sure glowed \nand lit up the night using python!")
