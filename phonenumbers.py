f08aa6863ee9c2e170e4c3c76676b555d404fda1
PalakJoshi04 <palakjoshi1993@gmail.com>
2017-07-07 13:29:30 -0400

    Update phonenumbers.py

--- phonenumbers.py
+++ phonenumbers.py
@@ -1,3 +1,4 @@
+# write a prog to create dictionary of phone nos, perform following operation for one member: edit, del,add
 phonebook = { 'John' : 123, 'Jack': 234, 'Jill' : '456'}
 print(phonebook)
 phonebook['Jill'] = '458'
