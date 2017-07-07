ae6cbb2049520c02fb83e0ece435647f65046ea6
PalakJoshi04 <palakjoshi1993@gmail.com>
2017-07-07 11:31:33 -0400

    Add files via upload
    practice for displaying text

---
+++ module2displayingtext.py
@@ -0,0 +1,4 @@
+print('I am a student from San Jose State University')
+print('I graduatedand i have a masters degree in EE')
+print("I am from India")
+print("I recently moved to Maryland from California")
