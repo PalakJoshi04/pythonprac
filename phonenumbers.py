2c871454b8e8f320e256f4c178c9c158f95de698
PalakJoshi04 <palakjoshi1993@gmail.com>
2017-07-07 13:28:29 -0400

    Add files via upload

---
+++ phonenumbers.py
@@ -0,0 +1,8 @@
+phonebook = { 'John' : 123, 'Jack': 234, 'Jill' : '456'}
+print(phonebook)
+phonebook['Jill'] = '458'
+print(phonebook)
+del phonebook['Jill']
+print(phonebook)
+phonebook['jake'] = '126'
+print(phonebook)
