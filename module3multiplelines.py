e9e2a13892b94c45254dc492a91a454898cf5a61
PalakJoshi04 <palakjoshi1993@gmail.com>
2017-07-07 11:33:58 -0400

    Add files via upload
    displaying multiple lines together with /n

---
+++ module3multiplelines.py
@@ -0,0 +1,11 @@
+print('I am a student from San Jose State University\nI have a masters degree in EE\ni recently moves from california to Maryland')
+
+print("i have masters degree in EE")
+print("i have masters degree in \nEE")
+print("""i have masters degree
+in EE""")
+
+print("I am a student from SJSU" + "i have masters degree in EE")
+print("but this is \news for u")
+
+print("but this is \\news for u")
