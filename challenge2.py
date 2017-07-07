7a54d5f4087ff0e5f5d783b9e4ebf759a47413b6
PalakJoshi04 <palakjoshi1993@gmail.com>
2017-07-07 16:39:57 -0400

    Update challenge2.py

--- challenge2.py
+++ challenge2.py
@@ -1,8 +1,9 @@
+# write a code to get purchase price from the user and if the total is less than 50, add 10 or else no shipping is applied
 total = float(input('enter the total of purchase'))
 if total < 50 :
-    print('shipping will applied')
-    shipping = total + 10
-    print('shipping will be = ' + str(shipping))
+    print('10$ shipping will applied')
+    totalpurchase = total + 10
+    print('shipping will be = ' + str(totalpurchase))
 else:
      print('shipping is free')
 print('thanks for shopping')
