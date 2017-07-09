d84f0e8248c5bb859b21f419a3b9e9fa419d22d5
PalakJoshi04 <palakjoshi1993@gmail.com>
2017-07-09 18:27:42 -0400

    Add files via upload

---
+++ turtlemodule.py
@@ -0,0 +1,12 @@
+import turtle
+sides = 5
+turtle.color('red')
+for steps in range(sides):
+    turtle.forward(100)
+    turtle.right(360/sides)
+    for steps in range(sides):
+        turtle.forward(50)
+        turtle.left(360/sides)
+        for steps in range(sides):
+            turtle.forward(10)
+            turtle.right(360/sides)
